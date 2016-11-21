4a83a3f2850b15f217d773c32478104d06a88a5d
Adam Jezirski <adam@factom.com>
2016-11-21 12:52:51 +0100

    another set of test

--- api_objects/factom_cli_create.py
+++ api_objects/factom_cli_create.py
@@ -4,14 +4,14 @@ from helpers.factom_cli_methods import send_command_to_cli_and_receive_text
 
 class FactomCliCreate():
 
-    factom_cli_command = 'factom-cli ' #TODO get it globalized
-    factom_importaddress = "importaddress "
-    factom_newfctaddress = "newfctaddress "
-    factom_importwords = "importwords "
-    factom_balance = "balance "
-    factom_ecrate = "ecrate"
-    factom_newtx = "newtx "
-    factom_add_transaction_f_input = "addtxinput "
+    _factom_cli_command = 'factom-cli ' #TODO get it globalized
+    _factom_importaddress = "importaddress "
+    _factom_newfctaddress = "newfctaddress "
+    _factom_importwords = "importwords "
+    _factom_balance = "balance "
+    _factom_ecrate = "ecrate"
+    _factom_newtx = "newtx "
+    _factom_add_transaction_f_input = "addtxinput "
     factom_add_transaction_f_output = "addtxoutput "
     factomd_substract_fee_on_tx = "subtxfee "
     factom_sign_transaction = 'signtx '
@@ -19,13 +19,17 @@ class FactomCliCreate():
     factom_ack = "ack "
     factom_sendtx = "sendtx "
     factom_new_entry_address = "newecaddress"
+    factom_listadresses = "listaddresses"
+    factom_exportadresses = "exportadresses"
+    factom_list_local_txs = "listtxs tmp"
+    _factom_remove_tx = "rmtx "
 
     def import_address_from_factoid(self, address_to_import_from):
-        return send_command_to_cli_and_receive_text(''.join((self.factom_cli_command, self.factom_importaddress,
-                                                            address_to_import_from)))
+        return send_command_to_cli_and_receive_text(''.join((self._factom_cli_command, self._factom_importaddress,
+                                                             address_to_import_from)))
 
     def create_new_factoid_address(self):
-        return send_command_to_cli_and_receive_text(''.join((self.factom_cli_command, self.factom_newfctaddress)))
+        return send_command_to_cli_and_receive_text(''.join((self._factom_cli_command, self._factom_newfctaddress)))
 
     def import_words_from_koinify_into_wallet(self, words):
         '''
@@ -33,49 +37,62 @@ class FactomCliCreate():
         :param words: space sepparated 12 words from koinify
         :return: txt, factoid wallet address
         '''
-        return send_command_to_cli_and_receive_text(''.join((self.factom_cli_command, self.factom_importwords, words)))
+        return send_command_to_cli_and_receive_text(''.join((self._factom_cli_command, self._factom_importwords, words)))
 
     def check_waller_address_balance(self, address):
-        return send_command_to_cli_and_receive_text(''.join((self.factom_cli_command, self.factom_balance, address)))
+        return send_command_to_cli_and_receive_text(''.join((self._factom_cli_command, self._factom_balance, address)))
 
     def get_factom_change_entry_credit_conversion_rate(self):
-        return send_command_to_cli_and_receive_text(''.join((self.factom_cli_command, self.factom_ecrate)))
+        return send_command_to_cli_and_receive_text(''.join((self._factom_cli_command, self._factom_ecrate)))
 
     def create_new_transaction_in_wallet(self, transaction_name):
-        return send_command_to_cli_and_receive_text(''.join((self.factom_cli_command, self.factom_newtx, transaction_name)))
+        return send_command_to_cli_and_receive_text(''.join((self._factom_cli_command, self._factom_newtx, transaction_name)))
 
     def add_foactoid_input_to_transaction_in_wallet(self, transaction_name, wallet_address, amount):
-        return send_command_to_cli_and_receive_text(''.join((self.factom_cli_command,
-                                                             self.factom_add_transaction_f_input, transaction_name,
+        return send_command_to_cli_and_receive_text(''.join((self._factom_cli_command,
+                                                             self._factom_add_transaction_f_input, transaction_name,
                                                              ' ', wallet_address, ' ', amount)))
 
     def add_foactoid_output_to_transaction_in_wallet(self, transaction_name, wallet_address, amount):
-        return send_command_to_cli_and_receive_text(''.join((self.factom_cli_command,
+        return send_command_to_cli_and_receive_text(''.join((self._factom_cli_command,
                                                              self.factom_add_transaction_f_output, transaction_name,
                                                              ' ', wallet_address, ' ', amount)))
 
     def set_account_to_substract_fee_from_that_transaction(self, transaction_name, wallet_address):
-        return send_command_to_cli_and_receive_text(''.join((self.factom_cli_command, self.factomd_substract_fee_on_tx, transaction_name, ' ', wallet_address)))
+        return send_command_to_cli_and_receive_text(''.join((self._factom_cli_command, self.factomd_substract_fee_on_tx, transaction_name, ' ', wallet_address)))
 
     def sign_transaction_in_wallet(self, transaction_name):
-        return send_command_to_cli_and_receive_text((''.join((self.factom_cli_command, self.factom_sign_transaction, transaction_name))))
+        return send_command_to_cli_and_receive_text((''.join((self._factom_cli_command, self.factom_sign_transaction, transaction_name))))
 
     def compose_transactsion_and_return_transactoin_code(self, transaction_name):
-        transaction = json.loads(send_command_to_cli_and_receive_text(''.join((self.factom_cli_command, self.factom_composetx, transaction_name))))
+        transaction = json.loads(send_command_to_cli_and_receive_text(''.join((self._factom_cli_command, self.factom_composetx, transaction_name))))
         return transaction['params']['transaction']
 
     def compose_transactsion(self, transaction_name):
-        return send_command_to_cli_and_receive_text(''.join((self.factom_cli_command, self.factom_composetx, transaction_name)))
+        return send_command_to_cli_and_receive_text(''.join((self._factom_cli_command, self.factom_composetx, transaction_name)))
 
 
     def request_transaction_acknowledgement(self, transaction_hash):
-        return send_command_to_cli_and_receive_text(''.join((self.factom_cli_command, self.factom_ack, transaction_hash)))
+        return send_command_to_cli_and_receive_text(''.join((self._factom_cli_command, self.factom_ack, transaction_hash)))
 
 
     def send_transaction_and_recive_transaction_id(self, transaction_name):
-        transacton = send_command_to_cli_and_receive_text(''.join((self.factom_cli_command, self.factom_sendtx, transaction_name)))
+        transacton = send_command_to_cli_and_receive_text(''.join((self._factom_cli_command, self.factom_sendtx, transaction_name)))
         return transacton.split(' ')[1]
 
     def create_entry_credit_address(self):
-        return send_command_to_cli_and_receive_text(''.join((self.factom_cli_command, self.factom_new_entry_address)))
+        return send_command_to_cli_and_receive_text(''.join((self._factom_cli_command, self.factom_new_entry_address)))
+
+    def list_adresses(self):
+        return send_command_to_cli_and_receive_text(''.join((self._factom_cli_command, self.factom_listadresses)))
+
+    def export_addresses(self):
+        return send_command_to_cli_and_receive_text(''.join((self._factom_cli_command, self.factom_exportadresses)))
+
+    def list_local_transactions(self):
+        return send_command_to_cli_and_receive_text(''.join((self._factom_cli_command, self.factom_list_local_txs)))
+
+    def remove_transaction_from_wallet(self, transaction_name):
+        return send_command_to_cli_and_receive_text(''.join((self._factom_cli_command, self._factom_remove_tx, transaction_name)))
+
 
--- factom_cli_tests/test_create_factoid_wallet_addresses.py
+++ factom_cli_tests/test_create_factoid_wallet_addresses.py
@@ -14,11 +14,7 @@ class FactomCliEndToEndTest(unittest.TestCase):
         cls.second_address = cls.factom_cli_create.create_new_factoid_address()
         words = '"salute umbrella proud setup delay ginger practice split toss jewel tuition stool"'
         cls.third_address = cls.factom_cli_create.import_words_from_koinify_into_wallet(words)
-
-    def test_factom_ecrate(self):
-        #do we have ability to know entry credit convention rate before test?
-        ecrate = self.factom_cli_create.get_factom_change_entry_credit_conversion_rate()
-        self.assertEqual(ecrate.strip(), '0.006666', "Ecrate is not as expected and is value is: " + ecrate)
+        cls.ecrate = self.factom_cli_create.get_factom_change_entry_credit_conversion_rate()
 
     def test_alocate_founds_to_factoid_walled_address(self):
 
@@ -27,24 +23,23 @@ class FactomCliEndToEndTest(unittest.TestCase):
         balance3 = self.factom_cli_create.check_waller_address_balance(self.third_address)
         transaction_name = ''.join(random.choice(string.ascii_letters) for _ in range (5))
         self.factom_cli_create.create_new_transaction_in_wallet(transaction_name)
-        self.factom_cli_create.add_foactoid_input_to_transaction_in_wallet(transaction_name, self.first_address, '150')
-        self.factom_cli_create.add_foactoid_output_to_transaction_in_wallet(transaction_name, self.second_address, '150')
+        self.factom_cli_create.add_foactoid_input_to_transaction_in_wallet(transaction_name, self.first_address, '1')
+        self.factom_cli_create.add_foactoid_output_to_transaction_in_wallet(transaction_name, self.second_address, '1')
         self.factom_cli_create.set_account_to_substract_fee_from_that_transaction(transaction_name, self.second_address)
         self.factom_cli_create.sign_transaction_in_wallet(transaction_name)
         #compose transaction
         transaction_hash =  self.factom_cli_create.compose_transactsion_and_return_transactoin_code(transaction_name)
         self.assertFalse('Internal error: Transaction not found' in self.factom_cli_create.request_transaction_acknowledgement(transaction_hash), "Transaction is not found in system")
         transaction_id = self.factom_cli_create.send_transaction_and_recive_transaction_id(transaction_name)
-
-        #TODO uncomment when you have amount to send/you will cnow
-        #self._wait_for_ack()
+        self._wait_for_ack(transaction_id, 60)
 
         #test here balance after change
         balance1_after = self.factom_cli_create.check_waller_address_balance(self.first_address)
         balance2_after = self.factom_cli_create.check_waller_address_balance(self.second_address)
         balance3_after = self.factom_cli_create.check_waller_address_balance(self.third_address)
 
-        self.assertEquals(int(balance2) + 150, balance2_after, 'cash was not send to address: ' + self.second_address)
+
+        self.assertTrue(balance2_after is not 0, 'cash was not send to address: ' + self.second_address)
 
     def test_if_you_can_compose_wrong_transaction(self):
         self.assertTrue("Transaction name was not found" in self.factom_cli_create.compose_transactsion('not_egsisting_trans'), 'Not existing transaction was found in wallet')
@@ -53,21 +48,67 @@ class FactomCliEndToEndTest(unittest.TestCase):
         self.assertTrue('Undefined' in self.factom_cli_create.check_waller_address_balance('wrong_account'))
 
     def test_entry_credits_wallet(self):
-        entry_creds_wallet1 = self.factom_cli_create.factom_importaddress('Es2Rf7iM6PdsqfYCo3D1tnAR65SkLENyWJG1deUzpRMQmbh9F3eG')
+        entry_creds_wallet1 = self.factom_cli_create.import_address_from_factoid('Es2Rf7iM6PdsqfYCo3D1tnAR65SkLENyWJG1deUzpRMQmbh9F3eG')
         entry_creds_wallet2 = self.factom_cli_create.create_entry_credit_address()
+
+        self.factom_cli_create.export_addresses()
+        self.factom_cli_create.list_adresses()
 
         balance_1 = self.factom_cli_create.check_waller_address_balance(entry_creds_wallet1)
         balance_2 = self.factom_cli_create.check_waller_address_balance(entry_creds_wallet2)
 
         #TODO think about assertion
 
-    def create_transaction_with_no_inputs_outputs_and_entry_creds(self):
+
+    def test_create_transaction_with_no_inputs_outputs_and_entry_creds(self):
         transaction_name = ''.join(random.choice(string.ascii_letters) for _ in range(5))
         self.factom_cli_create.create_new_transaction_in_wallet(transaction_name)
-        print self.factom_cli_create.send_transaction_and_recive_transaction_id(transaction_name)
+        self.factom_cli_create.send_transaction_and_recive_transaction_id(transaction_name)
+        self.assertTrue(transaction_name not in self.factom_cli_create.list_local_transactions(), 'Transaction was created')
+
+    def test_delete_transaction(self):
+        transaction_name = ''.join(random.choice(string.ascii_letters) for _ in range(5))
+        self.factom_cli_create.create_new_transaction_in_wallet(transaction_name)
+        self.factom_cli_create.add_foactoid_input_to_transaction_in_wallet(transaction_name, self.first_address, '1')
+        self.factom_cli_create.add_foactoid_output_to_transaction_in_wallet(transaction_name, self.second_address, '1')
+        self.factom_cli_create.set_account_to_substract_fee_from_that_transaction(transaction_name, self.second_address)
+        self.assertTrue(transaction_name in self.factom_cli_create.list_local_transactions(), 'Transaction was created')
+        self.factom_cli_create.remove_transaction_from_wallet(transaction_name)
+        self.assertTrue(transaction_name not in self.factom_cli_create.list_local_transactions(), 'Transaction was not deleted')
+
+    def test_create_transaction_with_not_equal_input_and_output(self):
+        transaction_name = ''.join(random.choice(string.ascii_letters) for _ in range(5))
+        self.factom_cli_create.create_new_transaction_in_wallet(transaction_name)
+        self.factom_cli_create.sign_transaction_in_wallet(transaction_name)
+        self.factom_cli_create.add_foactoid_input_to_transaction_in_wallet(transaction_name, self.first_address, '1')
+        self.assertTrue("Inputs and outputs don't add up" in
+                        self.factom_cli_create.set_account_to_substract_fee_from_that_transaction(transaction_name, self.first_address
+                                                                                                  ), "Input and outpt are don't adding up, but errior is not displayed")
+        self.factom_cli_create.remove_transaction_from_wallet(transaction_name)
+        self.assertTrue(transaction_name not in self.factom_cli_create.list_local_transactions(),
+                        'Transaction was not deleted')
+
+    def test_add_input_larger_than_10_x_fee_to_correct_transaction(self):
+        transaction_name = ''.join(random.choice(string.ascii_letters) for _ in range(5))
+        self.factom_cli_create.create_new_transaction_in_wallet(transaction_name)
+        self.factom_cli_create.add_foactoid_input_to_transaction_in_wallet(transaction_name, self.first_address, '1')
+        self.factom_cli_create.add_foactoid_output_to_transaction_in_wallet(transaction_name, self.first_address, '1')
+        self.factom_cli_create.set_account_to_substract_fee_from_that_transaction(transaction_name, self.first_address)
+        self.factom_cli_create.add_foactoid_input_to_transaction_in_wallet(transaction_name, self.first_address, '10')
+        self.assertTrue('Overpaying Fee' in self.factom_cli_create.sign_transaction_in_wallet(transaction_name),
+                        'Was able to overpay fee')
+        self.factom_cli_create.remove_transaction_from_wallet(transaction_name)
+        self.assertTrue(transaction_name not in self.factom_cli_create.list_local_transactions(),
+                        'Transaction was not deleted')
+
+
+
+
+
+
 
     def _wait_for_ack(self, transaction_id, time_to_wait):
-        status = ''
+        status = 'not found'
         i = 0
         while "not found" in status and i < time_to_wait:
             status = self.factom_cli_create.request_transaction_acknowledgement(transaction_id)
