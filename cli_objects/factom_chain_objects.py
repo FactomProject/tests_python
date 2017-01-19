02a735da3968b64b2c824010d48a92722c4b4264
adam jezeirski <adam@factom.com>
2017-01-19 06:09:03 -0800

    cleaning

--- cli_objects/factom_chain_objects.py
+++ cli_objects/factom_chain_objects.py
@@ -23,20 +23,6 @@ class FactomChainObjects(FactomBaseObject):
         text = send_command_to_cli_and_receive_text(''.join((self._factom_cli_command, self._factomd_add_chain, ext_to_string + ' ', ecadress, ' < ', file_data)))
         return text.split('\n')[1].split(' ')[1]
 
-    def make_chain_from_binary_file_with_address_customisation(self, wallet_address, factomd_address, ecadress, file_data, *external_ids):
-        '''
-        Make chain from binary data, external_ids should be string. there is no limit on external ids
-        :param ecadress:
-        :param file_data:
-        :param external_ids:
-        :return:
-        '''
-        _gopath = os.environ['GOPATH']
-        _factom_cli_command = _gopath + '/src/github.com/FactomProject/factom-cli/factom-cli -w ' \
-                                        '' + wallet_address + ' -s ' + factomd_address + ' '
-        ext_to_string = ' '.join(['-n ' + s for s in external_ids])
-        text = send_command_to_cli_and_receive_text(''.join((_factom_cli_command, self._factomd_add_chain, ext_to_string + ' ', ecadress, ' < ', file_data)))
-        return text.split('\n')[1].split(' ')[1]
 
     def force_make_chain_from_binary_file_and_receiv_chain_id(self, ecadress, file_data, *external_ids):
         '''
@@ -58,42 +44,14 @@ class FactomChainObjects(FactomBaseObject):
         text = send_command_to_cli_and_receive_text(''.join((self._factom_cli_command, self._factom_get_heights)))
         return text.split('\n')[0].split(' ')[1]
 
-    def get_directory_block_height_from_head_with_server_address_customisation(self, wallet_address, factomd_address):
-        _gopath = os.environ['GOPATH']
-        _factom_cli_command = _gopath + '/src/github.com/FactomProject/factom-cli/factom-cli -w ' \
-                                        '' + wallet_address + ' -s ' + factomd_address + ' '
-        text = send_command_to_cli_and_receive_text(''.join((_factom_cli_command, self._factom_get_heights)))
-        return text.split('\n')[0].split(' ')[1]
-
     def add_entries_to_chain_and_receive_entry_hash(self, ecadress, file_data, chain_id, *external_ids):
         ext_to_string = ' '.join(['-n ' + s for s in external_ids])
         text = send_command_to_cli_and_receive_text(''.join(
             (self._factom_cli_command, self._factom_add_entries, ' -c ', chain_id , ' ', ext_to_string + ' ', ecadress, ' < ', file_data)))
         return text.split('\n')[2].split(' ')[1]
 
-    def add_entries_to_chain_and_receive_entry_hash_with_address_customisation(self, wallet_address, factomd_address, ecadress, file_data, chain_id, *external_ids):
-        _gopath = os.environ['GOPATH']
-        _factom_cli_command = _gopath + '/src/github.com/FactomProject/factom-cli/factom-cli -w ' \
-                                        '' + wallet_address + ' -s ' + factomd_address + ' '
-        ext_to_string = ' '.join(['-n ' + s for s in external_ids])
-        text = send_command_to_cli_and_receive_text(''.join(
-            (_factom_cli_command, self._factom_add_entries, ' -c ', chain_id, ' ', ext_to_string + ' ', ecadress,
-             ' < ', file_data)))
-        return text.split('\n')[2].split(' ')[1]
 
     def get_entries_by_hash(self, hash):
         text = send_command_to_cli_and_receive_text(''.join(
             (self._factom_cli_command, self._factom_get_entry, ' ', hash)))
         return text
-
-    def get_entries_by_hash_with_address_customisation(self, wallet_address, factomd_address, hash ):
-        _gopath = os.environ['GOPATH']
-        _factom_cli_command = _gopath + '/src/github.com/FactomProject/factom-cli/factom-cli -w ' \
-                                        '' + wallet_address + ' -s ' + factomd_address + ' '
-        text = send_command_to_cli_and_receive_text(''.join(
-            (_factom_cli_command, self._factom_get_entry, ' ', hash)))
-        return text
-
-
-
-
--- factom_cli_tests/sanity_prod_test.py
+++ factom_cli_tests/sanity_prod_test.py
@@ -20,13 +20,14 @@ class FactomCliEndToEndTest(unittest.TestCase):
 
     def test_make_chain_and_check_balance(self):
         path = os.path.join(os.path.dirname(__file__), self.data['test_file_path'])
-        height = self.factom_chain_object.get_directory_block_height_from_head_with_server_address_customisation(self.data['wallet_address'], self.data['factomd_1_address'])
+        height = self.factom_chain_object.get_directory_block_height_from_head()
         name_1 = create_random_string(5)
         name_2 = create_random_string(5)
-        chain_id = self.factom_chain_object.make_chain_from_binary_file_with_address_customisation(self.data['wallet_address'], self.data['factomd_1_address'], self.entry_creds_wallet1, path, name_1, name_2)
-        hash =  self.factom_chain_object.add_entries_to_chain_and_receive_entry_hash_with_address_customisation(self.data['wallet_address'], self.data['factomd_1_address'], self.entry_creds_wallet1, path, chain_id, name_1, name_2)
+        chain_id = self.factom_chain_object.make_chain_from_binary_file(self.entry_creds_wallet1, path, name_1, name_2)
+        hash =  self.factom_chain_object.add_entries_to_chain_and_receive_entry_hash(self.entry_creds_wallet1, path, chain_id, name_1, name_2)
         time.sleep(self.data['time_to_sleep'])
-        self.assertTrue('just_text' in self.factom_chain_object.get_entries_by_hash_with_address_customisation(self.data['wallet_address'], self.data['factomd_2_address'], hash), 'Cannot get entry')
-        second_height = self.factom_chain_object.get_directory_block_height_from_head_with_server_address_customisation(self.data['wallet_address'], self.data['factomd_2_address'])
+        self.factom_cli_create.factomd_address = self.data['factomd_2_address']
+        self.assertTrue('just_text' in self.factom_chain_object.get_entries_by_hash(hash), 'Cannot get entry')
+        second_height = self.factom_chain_object.get_directory_block_height_from_head()
         self.assertTrue(second_height > height, 'Block height is not adjusted')
 
