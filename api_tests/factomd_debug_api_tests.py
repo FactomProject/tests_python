083c2f03db8a2514b8c6bd829232e1e567e7ef1d
Veena <veena@factom.com>
2017-02-07 22:11:41 -0600

    removed print statements

--- api_tests/factomd_debug_api_tests.py
+++ api_tests/factomd_debug_api_tests.py
@@ -8,7 +8,7 @@ class FactomDebugAPITests(unittest.TestCase):
         self.factomd_debug_api = FactomDebugApiObjects()
 
 
-    def get_holding_messages_in_queue(self):
+    def test_get_holding_messages_in_queue(self):
         holding_queue_msgs = self.factomd_debug_api.get_holding_queue()
         self.assertFalse(re.search('Method not found',str(holding_queue_msgs)),"holding message api is not found")
         print(holding_queue_msgs)
@@ -23,17 +23,17 @@ class FactomDebugAPITests(unittest.TestCase):
                     fed_list.append(server)
         return fed_list
 
-    def get_audit_servers(self):
+    def test_get_audit_servers(self):
         audit_servers = self.factomd_debug_api.get_audit_servers()
         self.assertFalse(re.search('Method not found',str(audit_servers)), "get audit server is not found")
         print audit_servers
 
-    def get_network_info(self):
+    def test_get_network_info(self):
         network_info = self.factomd_debug_api.get_network_info()
         self.assertFalse(re.search('Method not found',str(network_info)), "get network info is not found")
         print network_info
 
-    def get_predictive_fer(self):
+    def test_get_predictive_fer(self):
         predictive_fer = self.factomd_debug_api.get_predictive_fer()
         self.assertFalse(re.search('Method not found',str(predictive_fer)), "get predictive fer is not found")
         print predictive_fer
