e565969ec8f4232cd85f112b28d826a049bdceac
Veena <veena@factom.com>
2017-02-16 15:21:07 -0600

    added calls for enterprise wallet

--- enterprisewallet_objects/factom_enterprisewallet_objects.py
+++ enterprisewallet_objects/factom_enterprisewallet_objects.py
@@ -6,29 +6,88 @@ class EnterpriseWalletObjects():
     data = read_data_from_json('addresses.json')
     enterprisewallet_address = data['enterprisewallet_address']
     url_get = 'http://'+ enterprisewallet_address + '/GET?request='
-    url_post = 'http://'+ enterprisewallet_address + '/POST?request=get-address&json='
+    url_post = 'http://'+ enterprisewallet_address + '/POST?request='
+    status = True
 
+    online = 'on'
     transactions = 'related-transactions'
     synced = 'synced'
     addresses = 'addresses'
+    getaddress = 'get-address'
+    addressnobal = 'addresses-no-bal'
+    balances = 'balances'
+    deladdress = 'delete-address'
+    addressnamechange = 'address-name-change'
+    displayprivatekey= 'display-private-key'
+    newfctaddress='generate-new-address-factoid'
+    newecaddress='generate-new-address-ec'
+
+    def send_post_request(self,request, payload):
+        r = requests.post("http://localhost:8091/POST?request=" + request + "&json=" + payload)
+        print r.text
+        return r.text
+
+
+    def send_get_request(self,request):
+        r = requests.get(self.url_get + request)
+        print r.text
+        return r.text
+
+
+    def get_status_enterprise_wallet(self):
+        result = self.send_get_request(self.online)
+        return result
 
     def get_transactions_from_enterprise(self):
         # http://localhost:8091/GET?request=related-transactions
-        input = self.transactions
-        r = requests.get(self.url_get + input)
-        return r.text
+        result = self.send_get_request(self.transactions)
+        return result
 
     def get_synched_status(self):
-        input = self.synced
-        r = requests.get(self.url_get + input)
-        return r.text
+        result = self.send_get_request(self.synced)
+        return result
 
     def get_addresses(self):
-        input = self.addresses
-        r = requests.get(self.url_get + input)
-        return r.text
+        result = self.send_get_request(self.addresses)
+        return result
 
     def get_address(self,address):
         payload = '{"Address":"' + address + '"}'
-        r = requests.post(self.url_post + payload)
-        return r.text
+        result = self.send_post_request(self.getaddress,payload)
+        return result
+
+    def get_address_only(self):
+        result = self.send_get_request(self.addressnobal)
+        return result
+
+    def get_balances(self):
+        result = self.send_get_request(self.balances)
+        return result
+
+    def change_address_name(self,address,name):
+        payload = '{"Address":"' + address + '","Name":"' + name + '"}'
+        result = self.send_post_request(self.addressnamechange,payload)
+        return result
+
+    def delete_address(self,address):
+        payload = '{"Address":"' + address + '"}'
+        result = self.send_post_request(self.deladdress,payload)
+        if result.find("Not a post valid request"):
+            self.status = False
+            result = "Failed"
+        return result
+
+    def display_private_key(self,address):
+        payload = '{"Address":"' + address + '"}'
+        result = self.send_post_request(self.display_private_key,payload)
+        return result
+
+    def generate_new_factoid_address(self,name):
+        payload = name
+        result = self.send_post_request(self.newfctaddress,payload)
+        return result
+
+    def generate_new_entrycredit_address(self,name):
+        payload = name
+        result = self.send_post_request(self.newecaddress,payload)
+        print result
--- enterprisewallet_tests/enterprisewallet_tests.py
+++ enterprisewallet_tests/enterprisewallet_tests.py
@@ -11,19 +11,55 @@ class FactomEnterpriseWalletTests(unittest.TestCase):
         self.enterprisewallet = EnterpriseWalletObjects()
         data = read_data_from_json('shared_test_data.json')
         self.address = data['factoid_public_wallet_address']
+        self.deladdress = data['enterprise_wallet_address']
+
+    def test_get_status_enterprise_wallet(self):
+        result = self.enterprisewallet.get_status_enterprise_wallet()
+        print result
 
     def test_list_alltransactions(self):
-        transactions = self.enterprisewallet.get_transactions_from_enterprise()
-        print transactions
+        result = self.enterprisewallet.get_transactions_from_enterprise()
+        print result
 
     def test_check_synched_status(self):
         result = self.enterprisewallet.get_synched_status()
         print result
 
-    def test_address(self):
+    def test_get_address(self):
         result = self.enterprisewallet.get_address(self.address)
         print result
 
-    def test_addresses(self):
+    def test_balance_of_all_addresses(self):
         result = self.enterprisewallet.get_addresses()
+        print result
+
+    def test_get_address_only(self):
+        result = self.enterprisewallet.get_address_only()
+        print result
+
+    def test_get_balances(self):
+        result = self.enterprisewallet.get_balances()
+        print result
+
+    def test_check_change_address_name(self):
+        name = "NameChange"
+        result = self.enterprisewallet.change_address_name(self.address,name)
+        print result
+
+    def test_delete_address(self):
+        result = self.enterprisewallet.delete_address(self.deladdress)
+        print result
+
+    def test_display_private_key(self):
+        result = self.enterprisewallet.display_private_key(self.address)
+        print result
+
+    def test_generate_new_factoid_address(self):
+        name = "NEW-FCT-ADDRESS-01"
+        result = self.enterprisewallet.generate_new_factoid_address(name)
+        print result
+
+    def test_generate_new_ec_address(self):
+        name = "NEW-EC-ADDRESS-01"
+        result = self.enterprisewallet.generate_new_entrycredit_address(name)
         print result
