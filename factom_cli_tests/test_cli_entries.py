767e0e6595e9d951da99c43e4d8c5ff5ff65b562
factom <drobertbaker@factom.org>
2017-02-16 16:57:09 -0600

    added entry fee checking

---
+++ factom_cli_tests/test_cli_entries.py
@@ -0,0 +1,81 @@
+import unittest
+import os
+
+from helpers.factom_cli_methods import send_command_to_cli_and_receive_text
+from cli_objects.factom_cli_create import FactomCliCreate
+from cli_objects.factom_chain_objects import FactomChainObjects
+from helpers.helpers import create_random_string, read_data_from_json, wait_for_ack
+from nose.plugins.attrib import attr
+
+@attr(fast=True)
+class FactomCliTransactionTest(unittest.TestCase):
+    data = read_data_from_json('shared_test_data.json')
+
+    def setUp(self):
+        self.factom_cli_create = FactomCliCreate()
+        self.factom_chain_object = FactomChainObjects()
+        self.first_address = self.factom_cli_create.import_address_from_factoid(self.data['factoid_wallet_address'])
+        self.ecrate = self.factom_cli_create.get_factom_change_entry_credit_conversion_rate()
+        self.entry_creds_wallet1 = self.factom_cli_create.import_address_from_factoid(
+            self.data['ec_wallet_address'])
+
+    def test_verify_entry_costs(self):
+        # create chain
+        name_1 = create_random_string(5)
+        name_2 = create_random_string(5)
+
+        i = 1
+        with open('output_file', 'wb') as fout:
+            fout.write(os.urandom(i))
+            path = fout.name
+
+        text = self.force_make_chain_from_binary_file(self.entry_creds_wallet1, path, name_1, name_2)
+        chain_id = text.split('\n')[1].split(' ')[1]
+        tx_id = text.split('\n')[0].split(' ')[1]
+        wait_for_ack(self, tx_id,100)
+
+        balance_last = self.factom_cli_create.check_wallet_address_balance(self.entry_creds_wallet1)
+
+        # write entries
+        while i < 10241:
+            # write smallest entry for fee amount
+            name_1 = create_random_string(5)
+            name_2 = create_random_string(5)
+            tx_id = self.add_entries_to_chain_and_receive_tx_id(self.entry_creds_wallet1, path, chain_id, name_1,
+                                                                name_2)
+            wait_for_ack(self, tx_id,100)
+            balance_1st = self.factom_cli_create.check_wallet_address_balance(self.entry_creds_wallet1)
+            self.assertEqual(int(balance_last), int(balance_1st) + (i - 1) / 1024 + 1, 'Incorrect charge for entry')
+
+            i += 1023
+            # write largest entry for fee amount
+            with open('output_file', 'a') as fout:
+                fout.write(os.urandom(1023))
+            tx_id = self.add_entries_to_chain_and_receive_tx_id(self.entry_creds_wallet1, path, chain_id, name_1, name_2)
+            wait_for_ack(self, tx_id,100)
+            balance_last = self.factom_cli_create.check_wallet_address_balance(self.entry_creds_wallet1)
+            self.assertEqual(int(balance_1st), int(balance_last) + (i - 1) / 1024 + 1, 'Incorrect charge for entry')
+
+            i += 1
+            with open('output_file', 'a') as fout:
+                fout.write(os.urandom(1))
+
+        os.remove(path)
+
+    def force_make_chain_from_binary_file(self, ecadress, file_data, *external_ids):
+        ext_to_string = ' '.join(['-n ' + s for s in external_ids])
+        text = send_command_to_cli_and_receive_text(''.join((self.factom_chain_object._factom_cli_command, self.factom_chain_object._factomd_add_chain, ' -f ', ext_to_string + ' ', ecadress, ' < ', file_data)))
+        return text
+
+    def add_entries_to_chain_and_receive_tx_id(self, ecaddress, file_data, chain_id, *external_ids):
+        ext_to_string = ' '.join(['-n ' + s for s in external_ids])
+        text = send_command_to_cli_and_receive_text(''.join((self.factom_chain_object._factom_cli_command, self.factom_chain_object._factom_add_entries, ' -f ', ' -c ', chain_id , ' ', ext_to_string + ' ',
+             ecaddress, ' < ', file_data)))
+        return text.split('\n')[0].split(' ')[1]
+
+
+
+
+
+
+
--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,8 +1,8 @@
 import random
 import string
-import os
 import json
 import os
+import time
 
 def create_random_string(char_nr):
     return ''.join(random.choice(string.ascii_letters) for _ in range(char_nr))
@@ -19,6 +19,15 @@ def read_data_from_json(file_name):
     with open(filename) as f:
         json_data = f.read()
         return json.loads(json_data)
+
+def wait_for_ack(self, transaction_id, time_to_wait):
+    status = 'not found'
+    i = 0
+    while "TransactionACK" not in status and i < time_to_wait:
+        status = self.factom_cli_create.request_transaction_acknowledgement(transaction_id)
+        time.sleep(1)
+        i += 1
+
 
 
 
