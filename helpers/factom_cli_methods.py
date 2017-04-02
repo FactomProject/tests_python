76848d9d7db1feeb7748023c8051569a2d4007d9
adam jezeirski <adam@factom.com>
2017-04-03 00:33:42 +0200

    avoid prints

--- helpers/factom_cli_methods.py
+++ helpers/factom_cli_methods.py
@@ -4,10 +4,7 @@ import logging
 
 def send_command_to_cli_and_receive_text(cli_command):
     ret = commands.getstatusoutput(cli_command)
-    print cli_command
-    print ret[1]
-    # logging.getLogger('cli_command').info(cli_command)
-    # logging.getLogger('cli_command').info(ret[1])
+    logging.getLogger('cli_command').info(cli_command)
     return ret[1]
 
 def get_data_dump_from_server(server_address):
