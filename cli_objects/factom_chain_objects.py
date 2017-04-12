d8d5c29e6bc72d5a9d84e8f5a7db9dbb664af8c3
factom <drobertbaker@factom.org>
2017-04-12 10:21:09 -0500

    addressed requested changes

--- cli_objects/factom_chain_objects.py
+++ cli_objects/factom_chain_objects.py
@@ -26,16 +26,7 @@ class FactomChainObjects(FactomBaseObject):
         flags = ''
         if kwargs:
             flags = ' '.join(kwargs['flag_list'])
-        return send_command_to_cli_and_receive_text(''.join((self._factom_cli_command, self._factomd_add_chain, ' ', flags, ' ',
-                                                      ext_to_string, ' ', ecaddress, ' < ', file_data)))
-
-    def compose_chain_from_binary_file(self, ecadress, file_data, external_id_with_flags_list, **kwargs):
-        ext_to_string = ' '.join(external_id_with_flags_list)
-        flags = ''
-        if kwargs:
-            flags = ' '.join(kwargs['flag_list'])
-        text = send_command_to_cli_and_receive_text(''.join((self._factom_cli_command, self._factomd_compose_chain, flags, ext_to_string + ' ', ecadress, ' < ', file_data)))
-        return text
+        return send_command_to_cli_and_receive_text(''.join((self._factom_cli_command, self._factomd_add_chain, ' ', flags, ' ', ext_to_string, ' ', ecaddress, ' < ', file_data)))
 
     def compose_chain_from_binary_file(self, ecadress, file_data, external_id_with_flags_list, **kwargs):
         ext_to_string = ' '.join(external_id_with_flags_list)
