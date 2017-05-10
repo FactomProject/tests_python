e030da79acfb285cc175a27605c30a02437c1df9
adam jezeirski <adam@factom.com>
2017-05-10 14:00:34 +0200

    small mistake fixes

--- factom_cli_tests/test_cli_chains.py
+++ factom_cli_tests/test_cli_chains.py
@@ -29,11 +29,6 @@ class FactomChainTests(unittest.TestCase):
         self.ecrate = self.factom_cli_create.get_factom_change_entry_credit_conversion_rate()
         self.entry_creds_wallet = self.factom_cli_create.import_address_from_factoid(
             self.data['ec_wallet_address'])
-        self.entry_creds_wallet100 = self.factom_cli_create.create_entry_credit_address()
-        text = self.factom_cli_create.buy_ec(self.first_address, self.entry_creds_wallet100, '100')
-        chain_dict = self.factom_chain_object.parse_chain_data(text)
-        tx_id = chain_dict['TxID']
-        wait_for_ack(tx_id, self.ACK_WAIT_TIME)
 
     def test_make_chain_with_wrong_address(self):
         path = os.path.join(os.path.dirname(__file__), self.data['test_file_path'])
@@ -136,7 +131,6 @@ class FactomChainTests(unittest.TestCase):
         name_2 = create_random_string(5)
         names_list = ['-n', name_1, '-n', name_2]
         text = self.factom_chain_object.compose_chain_from_binary_file(self.entry_creds_wallet100, path, names_list)
-        print 'text', text.split("{")
         start = text.find('"message":"') + 11
         end = text.find('"},"method', start)
         self.factomd_api_objects.commit_chain_by_message(text[start:end])
