8660fe718bcb4ccff60d957019c28a8dbbfbbea7
Veena Gondkar <veena@factom.org>
2017-06-27 16:08:16 -0500

    added testcase for current-minute

--- api_tests/api_tests.py
+++ api_tests/api_tests.py
@@ -3,20 +3,33 @@ import unittest
 
 from nose.plugins.attrib import attr
 
-@attr(fast=True)
+
+from helpers.factom_cli_methods import get_data_dump_from_nonansible_server
+from helpers.helpers import read_data_from_json
+
+@attr(api=True)
 class FactomChainTests(unittest.TestCase):
 
     def setUp(self):
         self.factom_api = FactomApiObjects()
+        data = read_data_from_json('addresses.json')
+        self.factomd_address = data['factomd_controlpanel']
 
-    def test_directory_blocks(self):
+    def ntest_directory_blocks(self):
         keymr = self.factom_api.get_directory_block_head()
         self.assertTrue('000000000000000000000000000000000000000000000000000000000000000a' ==
                         self.factom_api.get_directory_block_by_keymr(keymr)['chainid'])
-    def test_get_heights(self):
+    def ntest_get_heights(self):
         self.assertTrue('entryheight' in self.factom_api.get_heights())
 
-    def test_get_blocks_by_heights(self):
+    def ntest_get_blocks_by_heights(self):
         heights = self.factom_api.get_heights()
         directory_block_height = heights['directoryblockheight']
         self.assertTrue('keymr' in self.factom_api.get_directory_block_by_height(directory_block_height))
+
+    def test_get_current_minute(self):
+        result = self.factom_api.get_current_minute()
+        print result
+        current_minute = result['minute']
+        print (get_data_dump_from_nonansible_server(self.factomd_address))
+        print current_minute
