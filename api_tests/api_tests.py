1d52445ec467cba7789a6ccad24be5289cbd6639
Veena Gondkar <veena@factom.org>
2017-06-28 14:10:48 -0500

    added current minute from datadump in testcase of api

--- api_tests/api_tests.py
+++ api_tests/api_tests.py
@@ -2,7 +2,7 @@ from api_objects.factomd_api_objects import FactomApiObjects
 import unittest
 
 from nose.plugins.attrib import attr
-
+import re
 
 from helpers.factom_cli_methods import get_data_dump_from_nonansible_server
 from helpers.helpers import read_data_from_json
@@ -15,21 +15,22 @@ class FactomChainTests(unittest.TestCase):
         data = read_data_from_json('addresses.json')
         self.factomd_address = data['factomd_controlpanel']
 
-    def ntest_directory_blocks(self):
+    def test_directory_blocks(self):
         keymr = self.factom_api.get_directory_block_head()
         self.assertTrue('000000000000000000000000000000000000000000000000000000000000000a' ==
                         self.factom_api.get_directory_block_by_keymr(keymr)['chainid'])
-    def ntest_get_heights(self):
+    def test_get_heights(self):
         self.assertTrue('entryheight' in self.factom_api.get_heights())
 
-    def ntest_get_blocks_by_heights(self):
+    def test_get_blocks_by_heights(self):
         heights = self.factom_api.get_heights()
         directory_block_height = heights['directoryblockheight']
         self.assertTrue('keymr' in self.factom_api.get_directory_block_by_height(directory_block_height))
 
     def test_get_current_minute(self):
         result = self.factom_api.get_current_minute()
-        print result
         current_minute = result['minute']
-        print (get_data_dump_from_nonansible_server(self.factomd_address))
-        print current_minute
+        datadump = get_data_dump_from_nonansible_server(self.factomd_address)
+        datadumplist = datadump.split('/')
+        minute = datadumplist[4]
+        self.assertEqual(int(re.search('[0-9]', minute).group()),int(current_minute),"minutes are not matching")
