019d6c0c30217ff4cb419fd4842326f6f4549cae
Veena Gondkar <veena@factom.org>
2017-06-29 15:21:36 -0500

    removed the primary key

--- api_tests/test_api_getallentries.py
+++ api_tests/test_api_getallentries.py
@@ -78,9 +78,7 @@ class FactomAPIEntryTests(unittest.TestCase):
         create_table_ecblock(factomd_conn)
         self.factom_api.change_factomd_address(self.factomd_address_prod)
         height = self.factom_api.get_heights()
-        counter=1
         for i in range(0,25000):
-            counter += 1
             ecblock = self.factom_api.get_entry_credit_block_by_height(i)
             len_entries = len(ecblock['body']['entries'])
             logging.getLogger('api_command').info("Height = %s" % i)
@@ -91,8 +89,7 @@ class FactomAPIEntryTests(unittest.TestCase):
                     if 'entryhash' in entries:
                         entryhash = ecblock['body']['entries'][x]['entryhash']
                         credits = ecblock['body']['entries'][x]['credits']
-                        counter = "a" + str(counter)
-                        insert_to_ecblock(counter,factomd_conn,entryhash,credits,i)
+                        insert_to_ecblock(factomd_conn,entryhash,credits,i)
                         if i % 100 == 0:
                             commit_to_db(factomd_conn)
         cur = factomd_conn.cursor()
--- helpers/db_methods.py
+++ helpers/db_methods.py
@@ -8,16 +8,15 @@ def create_table(conn):
     conn.execute('create table if not exists chain_entries(id UNIQUE,entryhash,chainid,size INT,height INT)')
 
 def create_table_ecblock(conn):
-    conn.execute('create table if not exists ecblock(id UNIQUE,entryhash,credits INT,height INT)')
+    #conn.execute('create table if not exists ecblock(id UNIQUE,entryhash,credits INT,height INT)')
+    conn.execute('create table if not exists ecblock(entryhash,credits INT,height INT)')
 
-def insert_to_db(conn,id,entry_hash,chainid,size,height):
-    conn.execute('INSERT OR IGNORE INTO chain_entries(id,entryhash,chainid,size,height)  VALUES (?,?,?,?)',(id,str(entry_hash),str(chainid),size,height,))
+def insert_to_db(conn,entry_hash,chainid,size,height):
+    conn.execute('INSERT OR IGNORE INTO chain_entries(entryhash,chainid,size,height)  VALUES (?,?,?)',(str(entry_hash),str(chainid),size,height,))
 
 
-def insert_to_ecblock(conn,id,entryhash,credits,height):
-    conn.execute('INSERT OR IGNORE INTO ecblock(id,entryhash,credits,height)  VALUES (?,?,?,?)',
-                 (id,str(entryhash), credits,height,))
-
+def insert_to_ecblock(conn,entryhash,credits,height):
+    conn.execute('INSERT OR IGNORE INTO ecblock(entryhash,credits,height)  VALUES (?,?,?)',(entryhash, credits,height,))
 
 def commit_to_db(conn):
     conn.commit()
