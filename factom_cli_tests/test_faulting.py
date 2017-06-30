ec716817094f9345082bf49883a8de0119ed81c9
factom <drobertbaker@factom.org>
2017-06-30 10:18:33 -0500

    fghfghf

--- factom_cli_tests/test_faulting.py
+++ factom_cli_tests/test_faulting.py
@@ -22,20 +22,19 @@ class FactomTestFaulting(unittest.TestCase):
         :return:
         '''
         self.assertIn(
-            self.data['audit_1_hash'] + ' online', get_data_dump_from_server(self.data['default_server_address']))
+            self.data['audit_1_hash'] + ' online', get_data_dump_from_server(self.data['default_server_address']), 'Audit server ' + self.data['audit_1_hash'] + ' is not online so can\'t be faulted')
         send_command_to_cli_and_receive_text(self._faulting_command + self.data['audit'])
-        time.sleep(self.data['time_to_wait'])
-        self.assertIn(self.data['audit_1_hash'] + ' offline', get_data_dump_from_server(self.data['default_server_address']))
+        time.sleep(self.data['BLOCK_TIME'])
+        self.assertIn(self.data['audit_1_hash'] + ' offline', get_data_dump_from_server(self.data['default_server_address']), 'Audit server ' + self.data['audit_1_hash'] + ' not faulted')
 
     def test_fault_federated_server(self):
         '''
         Test that checking if federated server is faulted and if network is not stalled after
         :return:
         '''
-        self.assertTrue(
-            self.data['audit_2_hash'] + ' online' in get_data_dump_from_server(
-                self.data['default_server_address']))
+        self.assertIn(
+            self.data['audit_2_hash'] + ' online', get_data_dump_from_server(self.data['default_server_address']))
         send_command_to_cli_and_receive_text(self._faulting_command + self.data['federated'])
-        time.sleep(self.data['time_to_wait'])
+        time.sleep(self.data['BLOCK_TIME'])
         self.assertTrue(
             self.data['audit_1_hash'] + ' offline' and  self.data['master_hash'] + ' offline' in get_data_dump_from_server(self.data['default_server_address']))
