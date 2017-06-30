45fba8fb627a10c65c2f15bd8092aca6d32d3c8e
factom <drobertbaker@factom.org>
2017-06-30 17:31:40 -0500

    added test to check that node resyncs correctly

--- factom_cli_tests/test_syncing.py
+++ factom_cli_tests/test_syncing.py
@@ -12,10 +12,9 @@ from helpers.factom_cli_methods import send_command_to_cli_and_receive_text, get
 class FactomTestFaulting(unittest.TestCase):
     data_shared = read_data_from_json('shared_test_data.json')
     data_fault = read_data_from_json('faulting.json')
+    data_sync = read_data_from_json('syncing.json')
     _faulting_command = 'docker stop factom-factomd-'
     _restart_command = 'docker start factom-factomd-'
-    BLOCKS_TO_FALL_BEHIND = 2
-    SECONDS_TO_WAIT_FOR_SYNC = 6000
 
     def setUp(self):
         self.factom_cli_create = FactomCliCreate()
@@ -30,7 +29,7 @@ class FactomTestFaulting(unittest.TestCase):
         self.assertIn(self.data_fault['audit_1_hash'] + ' offline', get_data_dump_from_server(self.data_fault['default_server_address']), 'Audit server ' + self.data_fault['audit_1_hash'] + ' not faulted')
 
         # keep node stopped for a bit
-        for i in xrange(self.BLOCKS_TO_FALL_BEHIND): time.sleep(self.data_shared['BLOCKTIME'])
+        for i in xrange(self.data_sync['BLOCKS_TO_FALL_BEHIND']): time.sleep(self.data_shared['BLOCKTIME'])
 
         # restart node
         send_command_to_cli_and_receive_text(self._restart_command + self.data_fault['audit'])
@@ -40,11 +39,11 @@ class FactomTestFaulting(unittest.TestCase):
         # wait for node to resync
         synced = False
         start_time = time.time()
-        while (time.time() - start_time < self.SECONDS_TO_WAIT_FOR_SYNC):
+        while (time.time() - start_time < self.data_sync['SECONDS_TO_WAIT_FOR_SYNC']):
             heights = self.factom_chain_object.get_heights()
             directory_block_height = self.factom_chain_object.parse_transaction_data(heights)['DirectoryBlockHeight']
             leader_block_height = self.factom_chain_object.parse_transaction_data(heights)['LeaderHeight']
             if (directory_block_height == leader_block_height):
                 synced = True
                 break
-        self.assertTrue(synced, 'Node did not sync up within ' + str(self.SECONDS_TO_WAIT_FOR_SYNC) + ' seconds')
+        self.assertTrue(synced, 'Node did not sync up within ' + str(self.data_sync['SECONDS_TO_WAIT_FOR_SYNC']) + ' seconds')
