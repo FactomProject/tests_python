8721823c5549a9bbf11c3aa90e8504f0458120dd
factom <drobertbaker@factom.org>
2017-08-07 17:09:04 -0500

    resolved conflict

--- helpers/general_test_methods.py
+++ helpers/general_test_methods.py
@@ -28,7 +28,7 @@ def wait_for_entry_in_block(**kwargs):
 
 def fund_entry_credit_address(self, amount):
     # all entry credit addresses are funded from first_address
-    first_address = cli_create.import_address_from_factoid(data['factoid_wallet_address'])
+    first_address = cli_create.import_address(data['factoid_wallet_address'])
     unittest.TestCase.assertGreaterEqual(self, cli_create.check_wallet_address_balance(first_address), amount, 'Not enough factoids available to fund address')
     entry_credit_address = cli_create.create_entry_credit_address()
     text = cli_create.buy_entry_credits(first_address, entry_credit_address, str(amount))
