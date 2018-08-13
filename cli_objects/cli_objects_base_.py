f0425d490318998825379bb49ede61c126e46b7b
Veena Gondkar <veena@factom.org>
2018-08-13 13:56:58 -0500

    created branch to test on simulator

--- cli_objects/cli_objects_base_.py
+++ cli_objects/cli_objects_base_.py
@@ -4,13 +4,13 @@ class CLIObjectsBase():
     data = read_data_from_json('addresses.json')
     wallet_address = data['wallet_address']
     factomd_address = data['factomd_address']
-    factomd_address_2 = data['factomd_address_2']
+ #   factomd_address_2 = data['factomd_address_2']
 
-    _cli_command = '/srv/factom-cli -w ' \
+    _cli_command = 'factom-cli -w ' \
                                   '' + wallet_address +' -s ' + factomd_address +' '
 
     def change_factomd_address(self, value):
-        self._cli_command = '/srv/factom-cli -w ' \
+        self._cli_command = 'factom-cli -w ' \
                                                  '' + self.wallet_address + ' -s ' + value + ' '
 
 
