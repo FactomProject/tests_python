c17c4efa04d6f682cf1c34186fd5814f4a9cb430
Veena <veena@factom.com>
2018-11-09 11:03:54 -0600

    started debug api code

--- api_objects/api_objects_debug.py
+++ api_objects/api_objects_debug.py
@@ -40,3 +40,96 @@ class APIObjectsDebug():
         '''
         audit_servers = json.loads(self.send_get_request_with_method('audit-servers'))
         return audit_servers["result"]["AuditServers"]
+
+
+    def get_holding_queue(self):
+        '''
+        Show holding messages in the queue
+        :return: holding messages in the queue
+        '''
+        holding_queue = json.loads(self.send_get_request_with_method('holding-queue'))
+        return holding_queue
+
+    def get_network_info(self):
+        '''
+        Get the network information
+        :return: the network information
+        '''
+        network_info = json.loads(self.send_get_request_with_method('network-info'))
+        return network_info
+
+    def get_predictive_fer(self):
+        '''
+        Get the predicted future exchange rate
+        :return: Get the predicted future exchange rate
+        '''
+        predictive_fer = json.loads(self.send_get_request_with_method('predictive-fer'))
+        return predictive_fer
+
+    def get_configuration(self):
+        '''
+        Get the configuration from the config file
+        :return: Get the configuration from the config file
+        '''
+        configuration = json.loads(self.send_get_request_with_method('configuration'))
+        return configuration
+
+    def get_droprate(self):
+        '''
+        Get the drop rate of factomd
+        :return: Get the drop rate of factomd
+        '''
+        droprate = json.loads(self.send_get_request_with_method('drop-rate'))
+        return droprate
+
+    def set_droprate(self,droprate):
+        '''
+        Set the drop rate of factomd node
+        :return: Set the drop rate of factomd node
+        '''
+        delay = json.loads(self.send_get_request_with_params_dict('droprate', {'droprate': droprate})[0])
+        return delay
+
+    def get_currentminute(self):
+        '''
+        Get the current minute of factomd
+        :return: Get the current minute of factomd
+        '''
+        currentminute = json.loads(self.send_get_request_with_method('current-minute'))
+        return currentminute["result"]
+
+    def get_summary(self):
+        '''
+        Get the summary of factomd
+        :return: Get the summary of factomd
+        '''
+        summary = json.loads(self.send_get_request_with_method('summary'))
+        return summary
+
+    def get_delay(self):
+        '''
+        Get the delay of factomd node
+        :return: Get the delay of factomd
+        '''
+        delay = json.loads(self.send_get_request_with_method('delay'))
+        return delay
+
+    def set_delay(self,delay):
+        '''
+        Set the delay of factomd node
+        :return: Set the delay of factomd node
+        '''
+        delay = json.loads(self.send_get_request_with_params_dict('delay', {'delay': delay})[0])
+        return delay
+
+    def reload_configuration(self):
+        conf = json.loads(self.send_get_request_with_method('reload-configuration'))
+        return conf
+
+    def get_process_list(self):
+        processlist =  json.loads(self.send_get_request_with_method('process-list'))
+        return processlist
+
+    def get_messages_list(self):
+        messages = json.loads(self.send_get_request_with_method('messages'))
+        return messages
--- api_tests/api_tests_debug.py
+++ api_tests/api_tests_debug.py
@@ -3,8 +3,9 @@ import unittest, re
 from nose.plugins.attrib import attr
 from api_objects.api_objects_debug import APIObjectsDebug
 from helpers.helpers import read_data_from_json
+from helpers.cli_methods import get_data_dump_from_server
 
-@attr(fast=True)
+@attr(debug=True)
 class FactomDebugAPItests(unittest.TestCase):
     api_debug = APIObjectsDebug()
 
@@ -12,7 +13,9 @@ class FactomDebugAPItests(unittest.TestCase):
         data = read_data_from_json('addresses.json')
         self.factomd_address = data['factomd_address']
         self.controlpanel = read_data_from_json('faulting.json')
-
+        self.factomd_address_custom_list = [data['factomd_address_0'], data['factomd_address_1'], data['factomd_address_2'],
+                                       data['factomd_address_3'], data['factomd_address_4'], data['factomd_address_5'],
+                                       data['factomd_address_6']]
 
     def test_get_federated_servers(self):
         self.api_debug.change_factomd_address(self.factomd_address)
@@ -35,3 +38,123 @@ class FactomDebugAPItests(unittest.TestCase):
                     audit_list.append(server)
         return audit_list
 
+
+    def test_get_holding_messages_in_queue(self):
+        holding_queue_msgs = self.api_debug.get_holding_queue()
+        self.assertFalse(re.search('Method not found',str(holding_queue_msgs)),"holding message api is not found")
+        print(holding_queue_msgs)
+                
+        
+    def test_get_predictive_fer(self):
+        predictive_fer = self.api_debug.get_predictive_fer()
+        self.assertFalse(re.search('Method not found',str(predictive_fer)), "get predictive fer is not found")
+        print predictive_fer
+
+    def test_audit_servers(self):
+        self.parse_federated_audit_servers("Audit")
+
+
+    def test_federated_servers(self):
+        self.parse_federated_audit_servers("Fed")
+
+
+    def parse_federated_audit_servers(self,authority):
+        output = self.get_raw_data()
+        found = True
+        if authority == "Fed":
+            result = self.find_between(str(output),'FederatedServersStart===','FederatedServersEnd')
+            fed_audit_list = self.get_federated_servers()
+        else:
+            result = self.find_between(str(output), 'AuditServersStart===', 'AuditServersEnd')
+            result = result.replace("online","")
+            fed_audit_list = self.get_audit_servers()
+        server_list = result.split("\\n")
+        if len(fed_audit_list) > 0 and len(server_list) > 0:
+            self.assertTrue(len(fed_audit_list) == int(server_list[0]),"servers is not matching")
+            server_list.remove(server_list[0])
+            server_list.remove(server_list[len(server_list)-1])
+            for servers in server_list:
+                found = False
+                if servers.endswith("F"):
+                    servers = servers.replace("F","")
+                for fed_audit in fed_audit_list:
+                    if re.search(servers,str(fed_audit)):
+                        found = True
+                        break
+            self.assertTrue(found, "federated server identity is not matching")
+
+    def find_between(self, s, first, last):
+        try:
+            start = s.rindex( first ) + len( first )
+            end = s.index( last, start )
+            return (s[start:end]).replace(" ","")
+        except ValueError:
+            return ""
+
+    def test_get_configuration(self):
+        configuration = self.api_debug.get_configuration()
+        print configuration
+
+
+    def test_droprate(self):
+        droprate = self.api_debug.get_droprate()
+        print droprate
+
+
+    def test_currentminute(self):
+        currentminute = self.api_debug.get_currentminute()
+        print "current minute is %d" % currentminute['Minute']
+
+
+    def test_current_minute_on_all_nodes(self):
+      for factomd_address_custom in self.factomd_address_custom_list:
+          self.api_debug.change_factomd_address(self.factomd_address)
+          currentminute = self.api_debug.get_currentminute()
+          self.api_debug.change_factomd_address(factomd_address_custom)
+          currentminute_1 = self.api_debug.get_currentminute()
+          if (currentminute == currentminute_1):
+              print "current minute on node - %s and node %s is %d" % (
+              self.factomd_address, factomd_address_custom, currentminute['Minute'])
+          else:
+            print "mismatch in node %s (%d) and %s (%d) " % (self.factomd_address, currentminute['Minute'], factomd_address_custom, currentminute_1['Minute'])
+            #try again before giving up
+            time.sleep(5)
+            currentminute = self.api_debug.get_currentminute()
+            currentminute_1 = self.api_debug.get_currentminute()
+            self.assertTrue(currentminute == currentminute_1,
+                                "mismatch in node %s (%d) and %s (%d) " % (self.factomd_address,currentminute['Minute'], factomd_address_custom, currentminute_1['Minute']))
+
+    def get_raw_data(self):
+        result = get_data_dump_from_server(self.controlpanel['default_server_address'])
+        return result
+
+
+    def test_summary(self):
+        result = self.api_debug.get_summary()
+        print result
+
+    def test_get_delay(self):
+        result = self.api_debug.get_delay()
+        print result
+
+
+    def test_set_delay(self):
+        result = self.api_debug.set_delay('200')
+        print result
+
+    def test_set_droprate(self):
+        result = self.api_debug.set_droprate('10')
+        print result
+
+    def test_reload_configuration(self):
+        result = self.api_debug.reload_configuration()
+        print result
+
+    def test_process_list(self):
+        result = self.api_debug.get_process_list()
+        print result
+
+    def test_messages(self):
+        print "in messages"
+        result = self.api_debug.get_messages_list()
+        print result
